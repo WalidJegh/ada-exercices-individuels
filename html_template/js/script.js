62326d5b9f6eaa14133bfd45565978543e0d4645
Walid Jeghnine <jeghninewalid@outlook.fr>
2023-06-09 10:49:00 +0200

    calculate age with the month now

--- html_template/js/script.js
+++ html_template/js/script.js
@@ -5,13 +5,32 @@ const askName = () => {
   document.body.innerHTML += `<h2>${hello} <br/> </h2>`;
 };
 
-askName();
-
 const askBirthYear = () => {
   let dateanswer = prompt("Quel est votre année de naissance ?");
+  let monthanswer = prompt("Quel est votre mois de naissance ?");
   let age = 2023 - dateanswer;
+  let month = [
+    "janvier",
+    "février",
+    "mars",
+    "avril",
+    "mai",
+    "juin",
+    "juillet",
+    "août",
+    "septembre",
+    "octobre",
+    "novembre",
+    "décembre",
+  ];
+
+  if (month.indexOf(monthanswer) + 1 > 6) {
+    age = age - 1;
+  }
+
   document.body.innerHTML += `<h3> Vous avez ${age} ans</h3>`;
   //   console.log(age);
 };
 
+askName();
 askBirthYear();
